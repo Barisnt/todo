2244edd10139ec20355c02df8506daac919be242
Baris Yilmaz <baris.yilmaz@inventanalytics.com>
2023-08-13 21:12:23 +0300

    redux toolkit and counter

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,21 @@
-import { FluentProvider, webLightTheme, Button } from "@fluentui/react-components";
+import { FluentProvider, webLightTheme, Button, webDarkTheme } from "@fluentui/react-components";
+import { useAppDispatch, useAppSelector } from "./store/hooks";
+import { increment } from "./store/counter/counterSlice";
 
 const App = () => {
+  const dispatch = useAppDispatch();
+  const counterSelector = useAppSelector(state => state.counter.value);
   return (
     <FluentProvider theme={webLightTheme}>
-      <Button appearance="primary">Hello Fluent UI React</Button>
+      <Button
+        appearance="primary"
+        onClick={() => {
+          dispatch(increment());
+        }}
+      >
+        +
+      </Button>
+      {counterSelector}
     </FluentProvider>
   );
 };
